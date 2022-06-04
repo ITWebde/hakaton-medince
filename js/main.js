97e7ac7f56126dad1c42bfe0a04450783c34a92f
Bakhriddinov_:) <bakhriddinov_@MacBook-Bakhriddinov-.local>
2022-06-04 17:56:43 +0500

    hakaton medince

---
+++ js/main.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    scrollbar: {
+        el: ".swiper-scrollbar",
+        hide: true,
+    },
+    autoplay: {
+        delay: 6000,
+        disableOnInteraction: false,
+    }
+});
